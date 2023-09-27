d0ffdbb375bab8da4d564c05c37422c9a24ff14d
Kliv Spahiu <102724470+KlivSpahiu@users.noreply.github.com>
2023-09-27 08:08:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen ">
+      <h1 className="text-red-600 flex justify-center text-xl">
+        Beesoft components
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', '.storybook/layout.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
