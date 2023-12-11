d7ac80014aa7f839e7ae6f04d7e7c3195c7c8946
brandontrabon <brandon.trabon@gmail.com>
2023-12-11 17:35:30 +1000

    BC-9 - Made some basic changes to the debounce function.

--- src/functions/debounce.ts
+++ src/functions/debounce.ts
@@ -1,36 +1,33 @@
-//includs the function and an option to cancel it
-interface DebouncedFunction<T extends (...args: any[]) => void> {
+//includes the function and an option to cancel it
+interface DebouncedFunction<T extends (...args: unknown[]) => void> {
   debouncedFunction: (...args: Parameters<T>) => void;
   cancel: () => void;
 }
 
-export function debounce<T extends (...args: any[]) => void>(
-  func: T,
-  timeout: number
-): DebouncedFunction<T> {
+export function debounce<T extends (...args: unknown[]) => void>(func: T, timeout: number): DebouncedFunction<T> {
   let timer: ReturnType<typeof setTimeout> | null = null;
 
-  const debouncedFunction = function (...args: Parameters<T>): void {
-      // Clear the existing timer, if any
-      if (timer !== null) {
-          clearTimeout(timer);
-      }
+  const debouncedFunction = (...args: Parameters<T>) => {
+    // Clear the existing timer, if any
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
 
-      // Set a new timer
-      timer = setTimeout(() => {
-          // Call the actual function with the provided arguments
-          func(...args);
-          timer = null; // Reset the timer after the function is invoked
-      }, timeout);
+    // Set a new timer
+    timer = setTimeout(() => {
+      // Call the actual function with the provided arguments
+      func(...args);
+      timer = null; // Reset the timer after the function is invoked
+    }, timeout);
   };
 
-  const cancel = function (): void {
-      // If there is an existing timer, clear it
-      if (timer !== null) {
-          clearTimeout(timer);
-          timer = null; // Reset the timer after cancellation
-      }
+  debouncedFunction.cancel = () => {
+    // If there is an existing timer, clear it
+    if (timer !== null) {
+      clearTimeout(timer);
+      timer = null; // Reset the timer after cancellation
+    }
   };
 
-  return { debouncedFunction, cancel };
+  return { debouncedFunction };
 }
