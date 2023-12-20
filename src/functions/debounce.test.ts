8fc76d348cbb975f48164efced0fcbf4ab3a39d3
HelenELee <HelenELee3@outlook.com>
2023-12-20 21:18:32 +0800

    bc-9 added test for cancel, flush and elapsed time

--- src/functions/debounce.test.ts
+++ src/functions/debounce.test.ts
@@ -5,12 +5,48 @@ jest.useFakeTimers();
 describe('debounce function', () => {
   it('should NOT call function', () => {
     const originalFunction = jest.fn();
-    const debounced = debounce(originalFunction, 200);
+    const debouncedFunction = debounce(originalFunction, 200);
 
     // Call the debounced function
-    debounced.debouncedFunction('arg1', 'arg2');
+    debouncedFunction('call 1');
 
     // Ensure the original function is NOT called
-    expect(originalFunction).not.toHaveBeenCalledWith('arg1', 'arg2');
+    expect(originalFunction).not.toHaveBeenCalledWith('call 1');
+  });
+
+  it('should call function - flush', () => {
+    const originalFunction = jest.fn();
+    const debouncedFunction = debounce(originalFunction, 200);
+
+    // Call the debounced function
+    debouncedFunction('call 1');
+    debouncedFunction.flush();
+    // Ensure the original function is called
+    expect(originalFunction).toHaveBeenCalledWith('call 1');
+  });
+
+  it('should NOT call function - cancel', () => {
+    const originalFunction = jest.fn();
+    const debouncedFunction = debounce(originalFunction, 200);
+
+    // Call the debounced function
+    debouncedFunction('call 1');
+    debouncedFunction.cancel();
+
+    // Ensure the original function is called
+    expect(originalFunction).not.toHaveBeenCalledWith('call 1');
+  });
+
+  it('should call function - time elapsed', () => {
+    jest.useFakeTimers();
+    const originalFunction = jest.fn();
+    const debouncedFunction = debounce(originalFunction, 200);
+
+    // Call the debounced function
+    debouncedFunction('call 1');
+    // Forward time by 200 milliseconds
+    jest.advanceTimersByTime(200);
+    // Ensure the original function is called
+    expect(originalFunction).toHaveBeenCalledWith('call 1');
   });
 });
