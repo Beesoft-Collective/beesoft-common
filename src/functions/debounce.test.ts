82f37d65435f9a2abcb802c9686272e4ff8d8ee5
HelenELee <HelenELee3@outlook.com>
2023-12-30 14:33:57 +0800

    bc-9 : added type - OriginalFunctionType, function - delay, used beforeEach, imported DebouncedFunction, added time elapsed tests

--- src/functions/debounce.test.ts
+++ src/functions/debounce.test.ts
@@ -1,13 +1,22 @@
-import { debounce } from './debounce.ts';
-import { describe, expect, test, mock } from 'bun:test';
+import { debounce, DebouncedFunction } from './debounce.ts';
+import { describe, expect, test, mock, beforeEach } from 'bun:test';
+
+type OriginalFunctionType = (x: unknown) => unknown;
+
+function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 describe('debounce function', () => {
-  test('should NOT call function', () => {
-    const originalFunction = mock((x) => {
-      return x;
-    });
-    const debouncedFunction = debounce(originalFunction, 200);
+  let originalFunction: OriginalFunctionType;
+  let debouncedFunction: DebouncedFunction<OriginalFunctionType>;
 
+  beforeEach(() => {
+    originalFunction = mock((x) => x);
+    debouncedFunction = debounce(originalFunction, 200);
+  });
+
+  test('should NOT call function', () => {
     // Call the debounced function
     debouncedFunction('call 1');
 
@@ -16,12 +25,7 @@ describe('debounce function', () => {
   });
 
   test('should call function - flush', () => {
-    const originalFunction = mock((x) => {
-      return x;
-    });
-    const debouncedFunction = debounce(originalFunction, 200);
-
-    // Call the debounced function
+    // Call the debounced function and flush
     debouncedFunction('call 1');
     debouncedFunction.flush();
     // Ensure the original function is called
@@ -29,31 +33,42 @@ describe('debounce function', () => {
   });
 
   test('should NOT call function - cancel', () => {
-    const originalFunction = mock((x) => {
-      return x;
-    });
-    const debouncedFunction = debounce(originalFunction, 200);
-
-    // Call the debounced function
+    // Call the debounced function and cancel
     debouncedFunction('call 1');
     debouncedFunction.cancel();
 
-    // Ensure the original function is called
+    // Ensure the original function is NOT called
     expect(originalFunction).not.toHaveBeenCalledWith('call 1');
   });
 
-  // test('should call function - time elapsed', () => {
-  //   //const originalFunction = jest.fn();
-  //   const originalFunction = mock((x) => {
-  //     return x;
-  //   });
-  //   const debouncedFunction = debounce(originalFunction, 200);
+  test('should call function - time elapsed', async () => {
+    // Call the debounced function
+    debouncedFunction('call 1');
+    await delay(200);
 
-  //   // Call the debounced function
-  //   debouncedFunction('call 1');
-  //   // Forward time by 200 milliseconds
-  //   jest.advanceTimersByTime(200);
-  //   // Ensure the original function is called
-  //   expect(originalFunction).toHaveBeenCalledWith('call 1');
-  // });
+    // Ensure the original function is called
+    expect(originalFunction).toHaveBeenCalledWith('call 1');
+  });
+
+  test('should call LAST function invoked - time elapsed', async () => {
+    // Call the debounced functions
+    debouncedFunction('call 1');
+    debouncedFunction('call 2');
+    debouncedFunction('call 3');
+    await delay(200);
+
+    // Ensure the LAST function is called
+    expect(originalFunction).toHaveBeenCalledWith('call 3');
+  });
+
+  test('should NOT call first function if subsequent call made - time elapsed', async () => {
+    // Call the debounced functions
+    debouncedFunction('call 1');
+    debouncedFunction('call 2');
+    debouncedFunction('call 3');
+    await delay(200);
+
+    // Ensure the LAST function is called
+    expect(originalFunction).not.toHaveBeenCalledWith('call 1');
+  });
 });
