e07d063724d8c3e972841f9b9407f3e49cba4af4
brandontrabon <brandon.trabon@gmail.com>
2024-01-27 20:21:34 +1000

    BC-16 - Added the `useStateRef` hook from beesoft-components.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { cloneDeep } from './functions/clone-deep/clone-deep.ts';
 import { debounce } from './functions/debounce/debounce.ts';
 import deepEquals from 'fast-deep-equal';
 import { useDeepMemo } from './hooks/use-deep-memo.ts';
+import { useStateRef } from './hooks/use-state-ref.ts';
 
 // types
 import { JsonItem, JsonData } from './custom-types/json-data.ts';
 import { MakeRequired } from './custom-types/make-required.ts';
 import { TypeOrArray } from './custom-types/type-or-array.ts';
 
-export { cloneDeep, debounce, deepEquals, useDeepMemo };
+export { cloneDeep, debounce, deepEquals, useDeepMemo, useStateRef };
 
 export type { JsonData, JsonItem, MakeRequired, TypeOrArray };
