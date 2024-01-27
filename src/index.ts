9d365f8f6dbea57ea9e7615bccbfa30e5d1837d7
brandontrabon <brandon.trabon@gmail.com>
2024-01-27 15:19:19 +1000

    BC-1 - Added a number of custom types and added the release it settings file.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,11 @@ import { debounce } from './functions/debounce/debounce.ts';
 import deepEquals from 'fast-deep-equal';
 import { useDeepMemo } from './hooks/use-deep-memo.ts';
 
+// types
+import { JsonItem, JsonData } from './custom-types/json-data.ts';
+import { MakeRequired } from './custom-types/make-required.ts';
+import { TypeOrArray } from './custom-types/type-or-array.ts';
+
 export { cloneDeep, debounce, deepEquals, useDeepMemo };
+
+export type { JsonData, JsonItem, MakeRequired, TypeOrArray };
