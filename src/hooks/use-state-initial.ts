f09d46564f6b459dc2d915b6de88c6fa6d0a33a2
Brandon Trabon <brandon.trabon@gmail.com>
2024-02-03 15:36:43 +1000

    Feature/bc-17 use state initial (#16)
    * BC-17 - Added the new `useStateInitial` hook; this will be used to tell the caller whether the current version of the state is the initial version.
    
    * [automated commit] Release v0.1.4-0
    
    * BC-17 - Added a `useStateRefInitial` hook that will work the same as `useStateInitial` but with the added ref like `useStateRef`.
    
    * BC-17 - Had to fix a small part of the code and add a comment.
    
    * [automated commit] Release v0.1.4-1
    
    ---------
    
    Co-authored-by: brandontrabon <brandontrabon@users.noreply.github.com>

---
+++ src/hooks/use-state-initial.ts
@@ -0,0 +1,66 @@
+import { Dispatch, SetStateAction, useRef, useState } from 'react';
+
+const isStateFunction = <S>(setStateAction: SetStateAction<S>): setStateAction is (prevState: S) => S =>
+  typeof setStateAction === 'function';
+
+/**
+ * Wraps the actual state value in an object with an initial property that determines if this version of the state is
+ * the initial version or not.
+ */
+interface InitialState<T> {
+  value: T;
+  initial: boolean;
+}
+
+/**
+ * A hook that will tell the user if the current version of the state is the initial version or not. Unlike `setState`
+ * this version requires an initial value be set.
+ * @param {T} initialState - The initial value of the state.
+ * @returns {readonly [InitialState<T>, <T>(value: ((<T>(prevState: T) => T) | T)) => void]}
+ */
+const useStateInitial = <T>(initialState: T) => {
+  const [state, setState] = useState<InitialState<T>>({
+    value: initialState,
+    initial: true,
+  });
+  const lastState = useRef<T>(initialState);
+
+  const dispatcher: Dispatch<SetStateAction<T>> = (value) => {
+    if (!isStateFunction(value)) {
+      setState({
+        value: value,
+        initial: false,
+      });
+    } else {
+      const newValue = value(lastState.current);
+      lastState.current = newValue;
+
+      setState({
+        value: newValue,
+        initial: false,
+      });
+    }
+  };
+
+  return [state, dispatcher] as const;
+};
+
+/**
+ * A hook that will tell the user if the current version of the state and ref variables are the initial version or not.
+ * Unlike `setState` this version requires an initial value be set.
+ * @param {T} initialState
+ * @returns {readonly [InitialState<T>, <T>(value: ((<T>(prevState: T) => T) | T)) => void, React.MutableRefObject<InitialState<T>>]}
+ */
+const useStateRefInitial = <T>(initialState: T) => {
+  const [state, setState] = useStateInitial(initialState);
+  const ref = useRef<InitialState<T>>({
+    value: initialState,
+    initial: true,
+  });
+
+  if (ref.current !== state) ref.current = state;
+
+  return [state, setState, ref] as const;
+};
+
+export { useStateInitial, useStateRefInitial };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { cloneDeep } from './functions/clone-deep/clone-deep.ts';
 import { debounce } from './functions/debounce/debounce.ts';
 import deepEquals from 'fast-deep-equal';
 import { useDeepMemo } from './hooks/use-deep-memo.ts';
+import { useStateInitial, useStateRefInitial } from './hooks/use-state-initial.ts';
 import { useStateRef } from './hooks/use-state-ref.ts';
 
 // types
@@ -9,6 +10,6 @@ import { JsonItem, JsonData } from './custom-types/json-data.ts';
 import { MakeRequired } from './custom-types/make-required.ts';
 import { TypeOrArray } from './custom-types/type-or-array.ts';
 
-export { cloneDeep, debounce, deepEquals, useDeepMemo, useStateRef };
+export { cloneDeep, debounce, deepEquals, useDeepMemo, useStateInitial, useStateRefInitial, useStateRef };
 
 export type { JsonData, JsonItem, MakeRequired, TypeOrArray };
