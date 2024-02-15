c190ec399cc4080a2201b0a389f9937593655689
Brandon Trabon <brandon.trabon@gmail.com>
2024-02-15 22:39:00 +1000

    BC-18 - Created the `usePropertyChanged` hook. (#17)
    * BC-18 - Created the `usePropertyChanged` hook.
    
    * [automated commit] Release v0.1.6-0
    
    * BC-18 - Fixed the last value check since it would fail for booleans.
    
    * [automated commit] Release v0.1.6-1
    
    * BC-18 - Forgot to ! the deepEquals result.
    
    * [automated commit] Release v0.1.6-2
    
    * BC-18 - Fixed the previous value returning the current value.
    
    * [automated commit] Release v0.1.6-3
    
    ---------
    
    Co-authored-by: brandontrabon <brandontrabon@users.noreply.github.com>

---
+++ src/hooks/use-property-changed.ts
@@ -0,0 +1,27 @@
+import deepEquals from 'fast-deep-equal';
+import { useRef } from 'react';
+
+export interface PropertyChangedReturn<T> {
+  changed: boolean;
+  previousValue: T | undefined;
+}
+
+const usePropertyChanged = <T>(property: T): PropertyChangedReturn<T> => {
+  const lastValue = useRef<T>();
+  let changed = false;
+
+  if (lastValue.current !== undefined) {
+    changed = !deepEquals(lastValue.current, property);
+  }
+
+  const returnValue: PropertyChangedReturn<T> = {
+    changed,
+    previousValue: lastValue.current,
+  };
+
+  lastValue.current = property;
+
+  return returnValue;
+};
+
+export { usePropertyChanged };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { cloneDeep } from './functions/clone-deep/clone-deep.ts';
 import { debounce } from './functions/debounce/debounce.ts';
 import deepEquals from 'fast-deep-equal';
 import { useDeepMemo } from './hooks/use-deep-memo.ts';
+import { usePropertyChanged } from './hooks/use-property-changed.ts';
 import { useStateInitial, useStateRefInitial } from './hooks/use-state-initial.ts';
 import { useStateRef } from './hooks/use-state-ref.ts';
 
@@ -11,6 +12,15 @@ import { JsonItem, JsonData } from './custom-types/json-data.ts';
 import { MakeRequired } from './custom-types/make-required.ts';
 import { TypeOrArray } from './custom-types/type-or-array.ts';
 
-export { cloneDeep, debounce, deepEquals, useDeepMemo, useStateInitial, useStateRefInitial, useStateRef };
+export {
+  cloneDeep,
+  debounce,
+  deepEquals,
+  useDeepMemo,
+  usePropertyChanged,
+  useStateInitial,
+  useStateRefInitial,
+  useStateRef,
+};
 
 export type { InitialState, JsonData, JsonItem, MakeRequired, TypeOrArray };
